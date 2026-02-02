ef57047418c08e9d28805b3a60411b16772421f5
Dhinesh-2005 <prakashdinesh09@gmail.com>
2026-02-02 23:18:23 +0530

    Fix th packages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -77,9 +77,9 @@ const Navbar = () => {
     <AnimatePresence>
       {isMobileMenuOpen && (
         <motion.div
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -10 }}
+          initial={{ opacity: 0, x: -10 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -10 }}
           className="fixed top-16 right-0 w-full z-[9999] md:hidden bg-[#0a0a0b] border-b border-white/10"
         >
           <div className="px-4 py-4 space-y-3">
